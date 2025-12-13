4c442961e0395081ae3ac4aee8e9100040ac3edb
AmineElHablani <aminehableni1898@gmail.com>
2025-12-13 22:08:09 +0100

    modified .env.example

--- app/main.py
+++ app/main.py
@@ -7,9 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 if __name__ == "__main__":
-    # Create directories
-    os.makedirs("generated_images", exist_ok=True)
 
     # Run the app
-    # uvicorn.run("app.main_api:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
     uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
